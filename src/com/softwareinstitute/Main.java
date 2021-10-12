49bf2265399e018edca4ace908b963dcdaf33a76
Amy Watts <amywatts93@hotmail.com>
2021-10-12 15:33:34 +0100

    if else to main

--- src/com/softwareinstitute/Main.java
+++ src/com/softwareinstitute/Main.java
@@ -33,13 +33,22 @@ public class Main {
         //amount to buy
         double toBuy;
         toBuy = totalArea / tinPaint;
-        System.out.println("Amount you need to buy is " + toBuy);
+        System.out.println("Amount of tins you need to buy is " + toBuy);
 
         // area paint to cover
         double paintCover;
         paintCover = toBuy * tinPaint;
         System.out.println("Amount of wall it will cover is " + paintCover);
 
+
+    String message;
+    if (toBuy <= 2) {
+        message = "You can afford the paint";
+    }
+    else {
+        message = "This is too expensive!";
+    }
+    System.out.println(message);
 
     }
 }
