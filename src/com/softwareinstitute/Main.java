c2eb77f88f63de787731a3d8b0bdfb0d51303e6d
Amy Watts <amywatts93@hotmail.com>
2021-10-12 16:59:49 +0100

    while loop added

--- src/com/softwareinstitute/Main.java
+++ src/com/softwareinstitute/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
         //sum total area
         wallArea = (2 * (roomLength * roomWidth + roomLength * roomHeight + roomWidth * roomHeight));
-        double totalArea =  wallArea;
+        double totalArea = wallArea;
 
         //amount to buy
         double toBuy;
@@ -40,7 +40,7 @@ public class Main {
         paintCover = toBuy * tinPaint;
         System.out.println("Amount of wall it will cover is " + paintCover);
 
-
+/*
     String message;
     if (toBuy <= 2) {
         message = "You can afford the paint";
@@ -49,6 +49,50 @@ public class Main {
         message = "This is too expensive!";
     }
     System.out.println(message);
+*/
+        switch ((int) toBuy) {
+            case 0:
+                toBuy = 1;
+                System.out.println("You can afford the paint");
+                break;
+            case 1:
+                toBuy = 2;
+                System.out.println("You can afford the paint");
+                break;
+            case 2:
+                toBuy = 3;
+                System.out.println("This is too much!");
+                break;
+            case 3:
+                toBuy = 4;
+                System.out.println("This is too much!");
+                break;
+            case 4:
+                toBuy = 5;
+                System.out.println("This is too much!");
+                break;
+            case 5:
+                toBuy = 6;
+                System.out.println("This is too much!");
+                break;
+
+            default:
+                System.out.println("Something went wrong, unable to determine budget");
+        }
+
+        boolean goPro = true;
+        while (goPro == true) {
+
+            if (paintCover > 2000) {
+                System.out.println("You're going to need a professional!");
+            } else {
+                goPro = false;
+                System.out.println("You got this!");
+            }
+            break;
+        }
+
 
     }
 }
+
