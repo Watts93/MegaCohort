0504efd3747420efc455aa7d296d25a582a9b450
Amy Watts <amywatts93@hotmail.com>
2021-10-15 16:29:56 +0100

    test all book titles

--- src/main/java/oopTesting/test.java
+++ src/main/java/oopTesting/test.java
@@ -1,5 +1,6 @@
 package oopTesting;
 
+import com.softwareinstitute.Adult;
 import com.softwareinstitute.Childrens;
 import com.softwareinstitute.Teen;
 
@@ -20,5 +21,13 @@ public class test {
         Teen testTeens = new Teen("Twilight", "Twilight Audio");
         assertEquals("Not the same","Twilight", testTeens.getBookTitle());
     }
+
+
+    @Test
+    public void testAdult(){
+        Adult testAdults = new Adult("A Million Little Pieces", "A Million Little Pieces Audio");
+        assertEquals("The book is not the same", "A Million Little Pieces",testAdults.getBookTitle());
+    }
+
 
 }
